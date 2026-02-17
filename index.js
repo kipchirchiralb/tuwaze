3f5ff843c44c91c7e846238d8f7cdce951a63a1d
Albert <albertkipchirchir02@gmail.com>
2026-02-17 12:06:55 +0300

    submiting anonymous tips

--- index.js
+++ index.js
@@ -96,13 +96,33 @@ app.post("/auth", (req, res) => {
 app.get("/dashboard", (req, res) => {
   connection.query("SELECT * FROM users;", (dbError, queryResult) => {
     if (dbError) {
-      console.log("DB error occured: " + dbError.message);
+      res.status(500).send("Server Error!!");
     } else {
-      console.log(getGenderCount(queryResult));
-      res.render("dashboard.ejs", {
-        allUsers: queryResult,
-        maleFemaleCount: getGenderCount(queryResult),
-      });
+      connection.query("SELECT * FROM anonymous_tips;",(dbError2, tipsQueryResult) => {
+          if (dbError2) {
+            res.send("Server Error!!" + dbError2.message);
+          } else {
+            res.render("dashboard.ejs", {
+              allUsers: queryResult,
+              maleFemaleCount: getGenderCount(queryResult),
+              allTips: tipsQueryResult,
+            });
+          }
+        },
+      );
+    }
+  });
+});
+
+app.post("/tips", (req, res) => {
+  const insertNewTip = `INSERT INTO anonymous_tips(category,description) 
+                          VALUES('${req.body.category}','${req.body.description}');`;
+  connection.query(insertNewTip, (dbError) => {
+    if (dbError) {
+      console.log("DB error occured: " + dbError.message);
+      res.status(500).send("Server Error!!");
+    } else {
+      res.send("Tip submitted successfully!!");
     }
   });
 });
